74d181aa3be58dcb82ada2d60ecfe53b2e00f331
parthasarthi317-blip <parthasarthi317@gmail.com>
2026-06-21 23:06:32 +0530

    Week 3 OOPS completed

---
+++ Week_3/OOPS/TestCompartment.java
@@ -0,0 +1,70 @@
+package Summer_Assignment_2401920310071.Week_3.OOPS;
+
+abstract class Compartment {
+
+    public abstract String notice();
+}
+
+class FirstClass extends Compartment {
+
+    public String notice() {
+        return "First Class Compartment";
+    }
+}
+
+class Ladies extends Compartment {
+
+    public String notice() {
+        return "Ladies Compartment";
+    }
+}
+
+class General extends Compartment {
+
+    public String notice() {
+        return "General Compartment";
+    }
+}
+
+class Luggage extends Compartment {
+
+    public String notice() {
+        return "Luggage Compartment";
+    }
+}
+
+public class TestCompartment {
+
+    public static void main(String[] args) {
+
+        Compartment[] compartments = new Compartment[10];
+
+        for(int i = 0; i < 10; i++) {
+
+            int random = (int)(Math.random() * 4) + 1;
+
+            switch(random) {
+
+                case 1:
+                    compartments[i] = new FirstClass();
+                    break;
+
+                case 2:
+                    compartments[i] = new Ladies();
+                    break;
+
+                case 3:
+                    compartments[i] = new General();
+                    break;
+
+                case 4:
+                    compartments[i] = new Luggage();
+                    break;
+            }
+        }
+
+        for(int i = 0; i < 10; i++) {
+            System.out.println(compartments[i].notice());
+        }
+    }
+}
